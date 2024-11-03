620ad069f7dcc2571d63e15bb1f8b21f79d32825
Masume Çetin <masume.cetin@veripark.com>
2024-11-03 17:27:24 +0300

    Initial Commit

---
+++ LOTRExchange/LOTRExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRExchangeApp.swift
+//  LOTRExchange
+//
+//  Created by Masume Çetin on 3.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct LOTRExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
